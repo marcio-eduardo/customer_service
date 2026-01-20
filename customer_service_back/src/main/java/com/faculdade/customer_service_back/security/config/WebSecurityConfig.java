166656f25728f89239ce64e99615597ce0bb5a19
marcio-eduardo <marcioeduardo.ti@gmail.com>
2026-01-20 18:58:24 -0300

    Atualizado Roles

--- customer_service_back/src/main/java/com/faculdade/customer_service_back/security/config/WebSecurityConfig.java
+++ customer_service_back/src/main/java/com/faculdade/customer_service_back/security/config/WebSecurityConfig.java
@@ -66,7 +66,9 @@ public class WebSecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/test/**").permitAll()
+                        .requestMatchers("/actuator/**", "/").permitAll() // Permitir Health Checks
 
                         // **ALTERAÇÕES APLICADAS AQUI PARA TICKETS**
                         // 1. Permitir que qualquer usuário autenticado abra um chamado
