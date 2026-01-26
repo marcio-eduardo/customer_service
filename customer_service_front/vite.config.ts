d4f4910f4cd703eaa5a7ab4617880aa5cdeefe83
marcio-eduardo <marcioeduardo.ti@gmail.com>
2026-01-25 21:14:25 -0300

    Correção de bugs

--- customer_service_front/vite.config.ts
+++ customer_service_front/vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path' // Importe o módulo 'path' do Node.js
 
+// Em projetos do tipo "module", precisamos definir __dirname manualmente
+const __dirname = path.resolve();
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
